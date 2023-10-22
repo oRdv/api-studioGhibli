bc8807b5b8f7be5b513532e698fbb66a6ba41703
Tamires <tamiressenaisp@gmail.com>
2023-10-22 14:59:38 -0300

    iniciando js

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const url = 'https://ghibliapi.vercel.app';
+
+let id
+
+const filmes = 'https://ghibliapi.vercel.app/films/'
+
+const criarSections = (filmes) => {
+    
+    const text = document.createElement('div')
+    text.classList.add('text')
+
+    const titleGs = document.createElement('div')
+    titleGs.classList.add('titleGs')
+}
