aeac9aae74c53a47b1e3418be514d177c8aa15ec
oR <tamiressenaisp@gmail.com>
2023-10-27 14:32:35 -0300

    aaa

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 'use strict';
 
-const url = 'https://ghibliapi.vercel.app';
-
-let id
+let id = 'dc2e6bd1-8156-4886-adff-b39e6043af0c'
 
 const filmes = 'https://ghibliapi.vercel.app/films/'
 
-const criarSections = (filmes) => {
-    
-    const text = document.createElement('div')
-    text.classList.add('text')
+const criarSections = (filme) => {
 
-    const titleGs = document.createElement('div')
-    titleGs.classList.add('titleGs')
+    
+    const section = document.getElementById('movie-card')
+    section.style.backgroundImage = `url(${filme.movie_banner})`
+    
+    console.log(filme)
+    const characterContent = `
+    <div class="info-container">
+        <img src="./" alt="">
+        <div class="text">
+            <div class="titleGs">${filme.title}</div>
+            <div class="containe3ss">
+                <div class="textstudios">
+                    <p>${filme.description}</p>
+                </div>
+            </div>
+            <div class="see-more">
+                <p>See More</p>
+            </div>
+        </div>
+    </div>
+    `
+
+    section.insertAdjacentHTML('afterbegin', characterContent)
+
+
 }
+
+const pegarFilme = async() => {
+
+    fetch(`https://ghibliapi.vercel.app/films/${id}`)
+    .then(response => response.json())
+    .then(filme => criarSections(filme))
+
+}
+
+// asdasasas.addeventlist('click', () =>{
+
+//     id = filme.id
+//     pegarFilme()
+
+// })
+
+pegarFilme()
